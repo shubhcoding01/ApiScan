f37d771b885d6ddb2e08848fa7df1aad8cdb32d5
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2026-01-05 18:59:47 +0530

    Updated Config

--- backend/app/config.py
+++ backend/app/config.py
@@ -11,7 +11,7 @@
 #    DATABASE_URL: str = os.getenv(
 #        "DATABASE_URL",
         #"postgresql://apiscan:apiscan@localhost:5432#/apiscan"
-    )
+#    )
 #    SECRET_KEY: str = os.getenv("SECRET_KEY", #"change-me")
 #    ALGORITHM: str = "HS256"
 #    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
